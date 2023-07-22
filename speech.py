2ec7344923e3eca12dc5620d5b76cc95059ae625
guti10501 <afgutierrez4@gmail.com>
2023-07-22 20:53:21 +0900

    After test in ipython

--- speech.py
+++ speech.py
@@ -1,7 +1,10 @@
 import boto3
+#s3 = boto3.client('s3')
+s3 = boto3.resource('s3')
+import time
 
-def start_polly_synthesis_task(region, endpoint_url, output_format, bucket_name, s3_key_prefix, voice_id, tex
-t_file_path):
+def start_polly_synthesis_task(engine="neural", region='ap-northeast-1', endpoint_url='https://polly.ap-northeast-1.amazonaws.com/', output_format='mp3', 
+bucket_name='podcast-wellness-e1', s3_key_prefix='prueba', voice_id='Ruth', text_file_path='./input.txt'):
     # Create a Boto3 client for Polly
     polly_client = boto3.client('polly', region_name=region, endpoint_url=endpoint_url)
     # Read the text content from the file
@@ -9,6 +12,7 @@ t_file_path):
         text = text_file.read()
     # Start the speech synthesis task
     response = polly_client.start_speech_synthesis_task(
+        Engine=engine,
         OutputFormat=output_format,
         OutputS3BucketName=bucket_name,
         OutputS3KeyPrefix=s3_key_prefix,
@@ -18,17 +22,13 @@ t_file_path):
     return response['SynthesisTask']['TaskId'] 
 
 #pending: How to define the language?
-region = 'ap-northeast-1'
-endpoint_url = 'https://polly.ap-northeast-1.amazonaws.com/'
-output_format = 'mp3'
-bucket_name = 'podcast-wellness-e1' #a bucket was created manually. This can be automated
-s3_key_prefix = 'prueba' #This is a folder inside the bucket
-voice_id = 'Joanna'
-text_file_path = './input.txt' #The input in the current directory
-
-task_id = start_polly_synthesis_task(region, endpoint_url, output_format, bucket_name, s3_key_prefix, voice_id, text_file_path)
-
-print(f'Started Polly speech synthesis task with ID: {task_id}')
+#region = 'ap-northeast-1'
+#endpoint_url = 'https://polly.ap-northeast-1.amazonaws.com/'
+#output_format = 'mp3'
+#bucket_name = 'podcast-wellness-e1' #a bucket was created manually. This can be automated
+#s3_key_prefix = 'prueba' #This is a folder inside the bucket
+#voice_id = 'Joanna'
+#text_file_path = './input.txt' #The input in the current directory
 
 def check_task_status(region, task_id):
     # Create a Boto3 client for Polly
@@ -40,27 +40,39 @@ def check_task_status(region, task_id):
 
     return status
 
-while True:
-    status = check_task_status(region, task_id)
-    if status == 'completed':
-        print('Speech synthesis task is completed.')
-        break
-    elif status == 'failed':
-        print('Speech synthesis task failed.')
-        break
-    elif status == 'inProgress':
-        print('Speech synthesis task is still in progress. Waiting...')
-        time.sleep(5)  # Wait for a few seconds before checking again
-    else:
-        print('Unexpected task status:', status)
-        break
+def tts(engine="neural", region='ap-northeast-1', endpoint_url='https://polly.ap-northeast-1.amazonaws.com/', output_format='mp3', 
+bucket_name='podcast-wellness-e1', s3_key_prefix='prueba', voice_id='Ruth', text_file_path='./input.txt'):
+    task_id = start_polly_synthesis_task(engine, region, endpoint_url, output_format, bucket_name, s3_key_prefix, voice_id, text_file_path)
+    print(f'Started Polly speech synthesis task with ID: {task_id}')
+    while True:
+        status = check_task_status(region, task_id)
+        if status == 'completed':
+            print('Speech synthesis task is completed.')
+            break
+        elif status == 'failed':
+            print('Speech synthesis task failed.')
+            break
+        elif status == 'scheduled':
+            print('Speech synthesis task is in scheduled.')
+            time.sleep(5) #wait
+        elif status == 'inProgress':
+            print('Speech synthesis task is still in progress. Waiting...')
+            time.sleep(5)  # Wait for a few seconds before checking again
+        else:
+            print('Unexpected task status:', status)
+            break
+    your_bucket = s3.Bucket(bucket_name)
+    for s3_file in your_bucket.objects.all():
+        print(s3_file.key)
+    your_bucket.download_file(f'{s3_key_prefix}.{task_id}.mp3','./output.mp3')
+    print(f'Successfully downloaded the output audio to: ./output.mp3')
+    return None
+    
 
-your_bucket = s3.Bucket(bucket_name)
 
-for s3_file in your_bucket.objects.all():
-    print(s3_file.key)
 
-your_bucket.download_file('prueba.f7869603-2cfa-47de-964e-c32bc6942e4c.mp3','./salida.mp3')
 
-your_bucket.download_file(f'{s3_key_prefix}.{task_id}.mp3','./output.mp3')
+
+
+
 
